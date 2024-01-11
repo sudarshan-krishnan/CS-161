960904519e351f3cdf04e4bdf1a1dcdc2f1a7e66
Sudarshan <k.sudarshan2904@gmail.com>
2024-01-11 11:15:04 -0500

    main

--- lab1-rectangle-grade/src/main/java/rectanglegraphic/Rectangle.java
+++ lab1-rectangle-grade/src/main/java/rectanglegraphic/Rectangle.java
@@ -1,5 +1,17 @@
+/*
+ * Name : Sudarshan Krishnan
+ * Username : kriss03
+ */
+
 package rectanglegraphic;
 
 public class Rectangle {
+
+    int cols;
+    int rows;
+    boolean filled;
     
+    public static void main(String[] args) {
+        
+    }
 }
--- lab1-rectangle-grade/src/main/java/rectanglegraphic/RectangleGraphic.java
+++ lab1-rectangle-grade/src/main/java/rectanglegraphic/RectangleGraphic.java
@@ -1,5 +1,14 @@
+/*
+ * Name : Sudarshan Krishnan
+ * Username : kriss03
+ */
+
 package rectanglegraphic;
 
 public class RectangleGraphic {
+    
+    public static void main(String[] args) {
+        
+    }
     
 }
