2fca2a28bd3b714cc0db743fb614cf872667c96e
Sudarshan <k.sudarshan2904@gmail.com>
2024-01-19 15:16:15 -0500

    eureka

--- lab1-rectangle-grade/src/main/java/rectanglegraphic/Rectangle.java
+++ lab1-rectangle-grade/src/main/java/rectanglegraphic/Rectangle.java
@@ -5,20 +5,42 @@
 
 package rectanglegraphic;
 
-import java.io.File;
-import java.io.FileReader;
-
 public class Rectangle {
 
-    int cols;
     int rows;
+    int cols;
     boolean filled;
     
-    public static void main(String[] args) {
+    public Rectangle (int a,int b,boolean bool){
 
-        File Rectangle = new File("rectangles.txt");
+        rows = a;
+        cols = b;
+        filled = bool;
 
-        
-        
     }
+
+    public String toString() {
+
+        String pattern = "";
+        
+             for (int i = 0; i < rows; i++) {
+                
+                for (int j = 0; j < cols; j++) {
+                    
+                    if (i == 0 || j == 0 || i == (rows-1) || j == (cols-1) || filled)
+                        pattern += "#";
+                    
+                    else 
+                    pattern += " ";
+                }
+                
+                pattern += "\n";
+            }
+        
+        return pattern;
+
+
+            
+    }
+
 }
--- lab1-rectangle-grade/src/main/java/rectanglegraphic/RectangleGraphic.java
+++ lab1-rectangle-grade/src/main/java/rectanglegraphic/RectangleGraphic.java
@@ -5,10 +5,40 @@
 
 package rectanglegraphic;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;     //imported entire util package instead of scanner class
+
 public class RectangleGraphic {
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+
+        File data = new File("rectangles.txt");
+        try (Scanner keyboard = new Scanner(data)) {
+            
+            while (keyboard.hasNext()) {
+
+                int row = keyboard.nextInt();
+                int col = keyboard.nextInt();
+                String solid = keyboard.next();
+
+                boolean b = false;
+
+                if (solid .equals("filled"))
+                    b = true;
+
+            
+                Rectangle rect = new Rectangle(row,col,b);
+
+                String pat = rect.toString();
+
+                System.out.println(pat);
+
+            }
+        }
         
+
     }
     
 }
+ 
