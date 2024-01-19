0aebe43c03efc4de0d853c3259628a8995443cfe
Sudarshan <k.sudarshan2904@gmail.com>
2024-01-19 16:04:18 -0500

    halfway thru gradebook

---
+++ lab1-rectangle-grade/src/main/java/gradebook/Stirng.java
@@ -0,0 +1,5 @@
+package gradebook;
+
+public class Stirng {
+
+}
--- lab1-rectangle-grade/src/main/java/gradebook/Student.java
+++ lab1-rectangle-grade/src/main/java/gradebook/Student.java
@@ -6,5 +6,66 @@
 package gradebook;
 
 public class Student {
+
+    String name;
+    int[] scores = new int[5];
+
+    public Student(String name, int[] scores){
+
+        this.name = name;
+        this.scores = new int[scores.length];
+
+        for (int i=0 ; i < scores.length ; i++){
+
+            this.scores[i] = scores[i];
+        }
+
+    }
+
+    public double getAverage(){
+
+        double sum = 0.0;
+
+        for(int i=0 ; i < this.scores.length ; i++){
+
+            sum += this.scores[i];
+
+        }
+
+        double average = sum/this.scores.length;
+
+        return average; 
+
+    }
+
+    public String getGrade(){
+
+        double avg = getAverage();
+
+        String grade = "";
+        
+                if (avg >= 90 && avg <= 100) {
+                    grade = "A";
+                } 
+                
+                else if (avg >= 80 && avg < 90) {
+                    grade = "B";
+                } 
+                
+                else if (avg >= 70 && avg < 80) {
+                    grade = "C";
+                } 
+                
+                else if (avg >= 60 && avg < 70) {
+                    grade = "D";
+                } 
+                
+                else {
+                    grade = "E";
+                }
+
+            return grade;
     
+            }
 }
+
