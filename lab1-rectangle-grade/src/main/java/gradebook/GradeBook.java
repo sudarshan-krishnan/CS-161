bfff6aafb35ebd5cc91975723ee3c36fad13241e
Sudarshan <k.sudarshan2904@gmail.com>
2024-01-20 15:40:05 -0500

    tommy can suck it

--- lab1-rectangle-grade/src/main/java/gradebook/GradeBook.java
+++ lab1-rectangle-grade/src/main/java/gradebook/GradeBook.java
@@ -12,51 +12,51 @@ import java.util.Scanner;
 
 public class GradeBook {
 
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws FileNotFoundException {   
         
         File first = new File("grade_data.txt");
-        Scanner reader = new Scanner(first);
+        try (Scanner reader = new Scanner(first)) {
+            int size = reader.nextInt();
 
-        int size = reader.nextInt();
+            Student[] obj = new Student[size];
 
-        Student[] obj = new Student[size];
+            for (int i = 0; i < size ; i++) {
 
-        for (int i = 0; i < size ; i++) {
+                String name = reader.next();
 
-            String name = reader.next();
+                int[] scores = new int[5];
 
-            int[] scores = new int[5];
+                for (int j = 0; j < 5; j++) {
 
-            for (int j = 0; j < 5; j++) {
+                    scores[j] = reader.nextInt(); 
+                    
+                }
 
-                scores[j] = reader.nextInt(); 
-                
+                obj[i] = new Student(name, scores);
+
             }
 
-            obj[i] = new Student(name, scores);
+            PrintWriter writer = new PrintWriter("gradebook.txt");
 
+            writer.println("Student\tTest1\tTest2\tTest3\tTest4\tTest5\tAverage\tGrade\n");
+
+            double summ = 0.0;
+            
+            for (int i = 0; i < obj.length ; i++) {
+
+                Student obj2 = obj[i];
+                writer.printf("%s%.1f\t%s%n",obj2.toString(),obj2.getAverage(),obj2.getGrade());
+
+                summ += obj2.getAverage();
+
+            }
+
+            double avgg = summ/size;
+            
+            writer.printf("%nClass Average = %.2f%n", avgg);
+
+            writer.close();
         }
-
-        PrintWriter writer = new PrintWriter("gradebook.txt");
-
-        writer.println("Student\tTest1\tTest2\tTest3\tTest4\tTest5\tAverage\tGrade\n");
-
-        double summ = 0.0;
-        
-        for (int i = 0; i < obj.length ; i++) {
-
-            Student obj2 = obj[i];
-            writer.printf("%s\t%.0f\t%s%n",obj2.toString(),obj2.getAverage(),obj2.getGrade());
-
-            summ += obj2.getAverage();
-
-        }
-
-        double avgg = summ/size;
-        
-        writer.printf("Class Average = %.2f%n", avgg);
-
-        writer.close();
 
     }
     
