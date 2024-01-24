092380fc2760285241162422fc9834207ee9fdba
Sudarshan <k.sudarshan2904@gmail.com>
2024-01-24 04:40:59 -0500

    added a couple classes

--- Lab2Party/src/main/java/Hero.java
+++ Lab2Party/src/main/java/Hero.java
@@ -4,5 +4,29 @@
 */
 
 public class Hero {
+
+	String name = "";
+	String role = "";
+	int level;
+	int experience;
+	final int MAX_LEVEL = 100;
+
+	public static final String[] ROLES = {"Warrior", "Priest", "Wizard", "Thief"};
+
+	public Hero (String nam) {
+
+		level = 1;
+		experience = 0;
+		role = "Unassigned";
+		this.name = nam;
+
+	}
+
+	public void setRole (String duty) {
+
+		
+
+	}
+
 	
 }
