1e75b8f590cc8439cc4f232a2c4811891bf887d8
Sudarshan <k.sudarshan2904@gmail.com>
2024-01-25 18:45:16 -0500

    smt i did

--- Lab2Party/src/main/java/Hero.java
+++ Lab2Party/src/main/java/Hero.java
@@ -9,11 +9,12 @@ public class Hero {
 	String role = "";
 	int level;
 	int experience;
+
 	final int MAX_LEVEL = 100;
 
-	public static final String[] ROLES = {"Warrior", "Priest", "Wizard", "Thief"};
+	public static final String[] ROLES = { "Warrior", "Priest", "Wizard", "Thief" };
 
-	public Hero (String nam) {
+	public Hero(String nam) {
 
 		level = 1;
 		experience = 0;
@@ -22,11 +23,65 @@ public class Hero {
 
 	}
 
-	public void setRole (String duty) {
+	public void setRole(String duty) {
+		boolean bool = false;
 
-		
+		for (int index = 0; index < ROLES.length; index++) {
+			if (ROLES[index].equals(duty)) {
+				this.role = duty;
+				bool = true;
+				break; // Exit the loop once a valid role is found
+			}
+		}
+
+		if (!bool) {
+			System.out.println("Invalid role");
+			role = "Unassigned";
+		}
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public int getLevel() {
+		return level;
+	}
+
+	public int getExperience() {
+		return experience;
+	}
+
+	public int expToLevelUp() {
+		int r = (int) Math.pow(level, 2);
+		return r;
+	}
+
+	public int gainExperience(int e) {
+
+		experience += e;
+
+		while (level < MAX_LEVEL && experience >= this.expToLevelUp()) {
+
+			experience -= this.expToLevelUp();
+			level++;
+
+			System.out.println(name + " is now level " + level + "!");
+
+		}
+		return level;
+	}
+
+	public String toString() {
+
+		String s = "";
+		return this.name + " the " + this.role + " is level " + this.level + " with " + this.experience
+				+ " experience.";
 
 	}
 
-	
 }
--- Lab2Party/src/main/java/Party.java
+++ Lab2Party/src/main/java/Party.java
@@ -1,8 +1,56 @@
 /*
-	Name: 
-	Username: 
+	Name: Sudarshan Krishnan
+	Username: kriss03
 */
 
 public class Party {
 
+	Hero[] heroes;
+
+	public Party() {
+		this.heroes = new Hero[3]; 
+
+	}
+
+	public void addHero(Hero hero ,int integer){
+
+        for (int i = 0; i < heroes.length; i++) {
+			Hero h = heroes[i]; // Getting the hero at the current index
+				
+				// Checking if names match
+				if (h.getName().equals(hero.getName())) {
+					System.out.println(hero.getName() + " is already in the party.");
+					return; 
+				}
+			}
+			heroes[integer] = hero;
+
+		}
+
+	public void removeHero(int index) {
+		heroes[index] = null;
+	}
+ 
+	public Hero getHero(int index) {
+		return heroes[index];
+	}
+
+    public void gainExperience(int experience) {
+        System.out.println("The party gained " + experience + " experience.");
+        for (Hero hero : heroes) {
+            if (hero != null) {
+                hero.gainExperience(experience);
+            }
+        }
+    }
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder("Party:\n");
+        for (Hero hero : heroes) {
+            if (hero != null) {
+                sb.append(hero.toString()).append("\n");
+            }
+        }
+        return sb.toString();
+}
 }
